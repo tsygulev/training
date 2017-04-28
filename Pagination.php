260319b6e6b03938c9aa37172831fcb81a702078
Dmitry Khaperets <khaperets@gmail.com>
2017-04-28 19:29:44 +0300

    Implemented pagination logic Signed-off-by:Dmitry Khaperets <khaperets@gmail.com>

--- Pagination.php
+++ Pagination.php
@@ -2,4 +2,31 @@
 
 class Pagination implements PaginationInterface
 {
+    /**
+     * @var int
+     */
+    private $firstPage = 1;
+
+    /**
+     * @var int
+     */
+    private $endPage;
+
+    /**
+     * @param int $total
+     * @param int $limit
+     * @param int $currentPage
+     */
+    public function __construct(int $total, int $limit, int $currentPage)
+    {
+        $this->endPage = ceil($total / $limit);
+    }
+
+    /**
+     * @return array
+     */
+    public function render(): array
+    {
+        return range($this->firstPage, $this->endPage);
+    }
 }
