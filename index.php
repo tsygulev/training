c638612516977e27bce8ef55b11d5191a83b48cd
Tsygulev Ivan <tsygulev.ivan@gmail.com>
2017-04-30 18:52:35 +0300

    Impelemented new pagination interface

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ require_once './Pagination.php';
 
 $pagination = new Pagination(123, 7, 4);
 
-$pagination->setLimit(8);
 $pagination->setCurrentPage($_GET['page'] ?? 1);
 $pagination->setTotalItems(89);
 
-var_dump($pagination->render());
+echo '<pre>';
+var_dump($pagination->getPagesList());
+echo '</pre>';
